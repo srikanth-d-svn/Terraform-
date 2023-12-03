e80d8c9c71ec60f08981826fed75c2bccd800049
srikanth-d-svn <112198058+srikanth-d-svn@users.noreply.github.com>
2023-12-03 15:14:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "foo" {
-  ami           = "ami-02e94b011299ef128" 
+  ami           = "ami-02a2af70a66af6dfb" 
   instance_type = "t2.micro"
   tags = {
       Name = "TF-Instance"
