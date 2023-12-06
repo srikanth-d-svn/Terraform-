02bda58c8923411b5ceafee713181e3937fcb31c
srikanth-d-svn <112198058+srikanth-d-svn@users.noreply.github.com>
2023-12-06 15:07:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,14 @@
 provider "aws" {
-    region = "ap-south-1"  
+  region = "ap-south-1"
 }
 
-resource "aws_instance" "foo" {
-  ami           = "ami-02a2af70a66af6dfb" 
-  instance_type = "t2.micro"
-  tags = {
-      Name = "TF-Instance"
-  }
+resource "aws_instance" "example" {
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  availability_zone      = var.availability_zone
+  iam_instance_profile   = var.iam_instance_profile
+  key_name               = var.key_name
+  security_groups        = var.security_groups
+  subnet_id              = var.subnet_id
+  tags                   = var.tags
 }
